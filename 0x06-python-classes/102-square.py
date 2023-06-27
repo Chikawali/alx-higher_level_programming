ffc3f08aea5c8bb4fca7b37553173117fa192c0d
Chikawali <chykoblanko@gmail.com>
2023-06-27 09:58:20 -0700

    102-square.py

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -20,7 +20,7 @@ class Square:
     def size(self, value):
         if type(value) != int and type(value) != float:
             raise TypeError("size must be a number")
-        elif: value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
@@ -28,26 +28,26 @@ class Square:
     def area(self):
         return self.__size * self.__size
 
-    def __eq__(self, other):
+    def __eq__(self, value):
         """check if equal to another square"""
-        return(self.area() == other.area())
+        return(self.area() == value.area())
 
-    def __lt__(self, other):
+    def __lt__(self, value):
         """check if less than other square"""
-        return(self.area() < other.area())
+        return(self.area() < value.area())
 
-    def __le__(self, other):
+    def __le__(self, value):
         """check if less than or equal to other square"""
-        return(self.area() <= other.area())
+        return(self.area() <= value.area())
 
-    def __ne__(self, other):
+    def __ne__(self, value):
         """check if not equal to another suqare"""
-        return(self.area() != other.area())
+        return(self.area() != value.area())
 
-    def __gt__(self, other):
+    def __gt__(self, value):
         """check if greater than another square"""
-        return(self.area() > other.area())
+        return(self.area() > value.area())
 
-    def __ge__(self, other):
+    def __ge__(self, value):
         """check if greater than or equal to another square"""
-        return(self.area() >= other.area())
+        return(self.area() >= value.area())
