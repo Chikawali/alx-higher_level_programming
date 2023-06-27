2c3da0ee9ecf0784bd0d53b2a2454dc98b4c0a9b
Chikawali <chykoblanko@gmail.com>
2023-06-27 09:45:10 -0700

    102-square.py

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -20,7 +20,7 @@ class Square:
     def size(self, value):
         if type(value) != int and type(value) != float:
             raise TypeError("size must be a number")
-        elif value < 0:
+        else: value < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
