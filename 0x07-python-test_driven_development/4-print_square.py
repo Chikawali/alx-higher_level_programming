da7ce0a4520bde59d5ae55f4f5a231e8ac774bd4
Chikawali <chykoblanko@gmail.com>
2023-07-07 08:49:58 -0700

    4-print_square.py

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,49 +1,21 @@
 #!/usr/bin/python3
+"""Defines a square-printing function."""
+
 
 def print_square(size):
-
-    """
-    # Write a function that prints a square with the character #.
-    # size is the size length of the square......
-    # size must be an integer, otherwise raise a TypeError,,,,,,
-    # exception with the message size must be an integer.......
-    # if size is less than 0, raise a ValueError exception with the....
-    # .....message size must be >= 0
-    # f size is a float and is less than 0, raise a TypeError....
-    # ....exception with the message size must be an integer......
-    # VARIABLE(" "):
-    # Print Square(Str): Print Square
-    # Return: Always 0, (Success).
-
-    Prints a square with the character '#' of the given size.
+    """Print a square with the # character.
 
     Args:
-        size (int): The size length of the square
-
+        size (int): The height/width of the square.
     Raises:
-        TypeError: If size is not an integer...
-        alueError: If size is less than 0.
-
-    Example:
-        print_square(3)
-        # Output:
-        # ###
-        # ###
-        # ###
-
-        print_square(5)
-        # Output:
-        # #####
-        # #####
-        # #####
-        # #####
-        # #####
+        TypeError: If size is not an integer.
+        ValueError: If size is < 0
     """
-
-    if type(size) is not int:
-        raise TypeError('size must be an integer')
-        return
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
     if size < 0:
-        raise ValueError('size must be >= 0')
+        raise ValueError("size must be >= 0")
+
     for i in range(size):
-        print("#" * size)
+        [print("#", end="") for j in range(size)]
+        print("")
