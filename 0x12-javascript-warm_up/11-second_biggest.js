d264ed65beae5329d36f3a31d9cc1bfcc59358f1
Chikawali <chykoblanko@gmail.com>
2023-09-11 16:58:17 -0700

    all done

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,20 +1,7 @@
 #!/usr/bin/node
-
-if (process.argv.length < 4) {
-  console.log('0');
+const argList = process.argv;
+if (argList.length <= 3) {
+  console.log(0);
 } else {
-  const arr = process.argv.slice(2);
-  let secondElement = Number.MIN_VALUE;
-  let maxValue = Number.MAX_VALUE;
-
-  arr.forEach(function (element) {
-    element = parseInt(element);
-    if (element > maxValue) {
-      secondElement = maxValue;
-      maxValue = element;
-    } else if (secondElement < element && maxValue > element) {
-      secondElement = element;
-    }
-  });
-  console.log(secondElement);
+  console.log(argList.sort().reverse()[1]);
 }
