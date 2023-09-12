5a7c9cbb3f72362f7ce5f1ab27640217b7c45b08
Chikawali <chykoblanko@gmail.com>
2023-09-11 17:01:25 -0700

    all done

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,12 @@
 #!/usr/bin/node
-const argList = process.argv;
-if (argList.length <= 3) {
-  console.log(0);
-} else {
-  console.log(argList.sort().reverse()[1]);
+
+const args = process.argv.slice(2);
+let nextMax = 0;
+
+if (args.length > 1) {
+  const integers = args.map(Number);
+  integers.sort((a, b) => b - a);
+  nextMax = integers[1];
 }
+
+console.log(nextMax);
